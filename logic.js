1b8ee8601bc13adf412bb33516e77133a8a279bd
Jim Schlader <jamesschlader@yahoo.com>
2019-04-23 13:21:31 -0600

    MVP done

--- logic.js
+++ logic.js
@@ -1,9 +1,15 @@
 const oneRandom = "https://dog.ceo/api/breeds/image/random";
 const breedList = "https://dog.ceo/api/breeds/list/all";
-let oneImage, breedButton, ratingBox, rating;
+const randomByBreed = breed =>
+  `https://dog.ceo/api/breed/${breed}/images/random`;
+let oneImage, breedButton, ratingBox, rating, favPic;
 const ratings = [10, 11, 12, 13, 14, 15, 16];
+const favs = [];
+let breeds;
 
 function getOne(url) {
+  let localFavs = JSON.parse(localStorage.getItem("favorites"));
+
   fetch(url, {
     method: "get",
     mode: "cors"
@@ -12,7 +18,10 @@ function getOne(url) {
       return res.json();
     })
     .then(res => {
-      oneImage = d3.select("#pics");
+      oneImage = d3
+        .select("#pics")
+        .append("div")
+        .attr("class", "random-image");
 
       oneImage
         .insert("img")
@@ -37,15 +46,15 @@ function getOne(url) {
         .append("button")
         .attr(
           "class",
-          "uk-button uk-button-default uk-margin-small uk-margin-right"
+          "uk-button uk-button-default uk-margin-small uk-margin-right uk-border-rounded"
         )
         .html(d => d)
         .on("click", d => {
           d3.event.preventDefault();
-          console.log(d);
+
           rating = d;
           d3.select("#display-rating").remove();
-          oneImage
+          ratingBox
             .append("h3")
             .attr("class", "uk-margin uk-margin-right")
             .attr("id", "display-rating")
@@ -55,32 +64,125 @@ function getOne(url) {
               "class",
               "uk-button uk-button-primary uk-margin uk-margin-left"
             )
-            .text("Save Doggie");
+            .text("Save Doggie")
+            .on("click", d => {
+              d3.event.preventDefault();
+
+              const newFav = {
+                rating: rating,
+                url: res.message
+              };
+              favs.push(newFav);
+              localFavs = [...localFavs, ...favs];
+              localStorage.setItem("favorites", JSON.stringify(localFavs));
+              d3.selectAll(".random-image").remove();
+              getFavs(localFavs);
+              getOne(oneRandom);
+            });
         });
 
       oneImage
         .append("button")
-        .attr("id", "breed-button")
-        .attr("class", "uk-button uk-margin-top uk-button-default")
+        .attr("id", "refresh-button")
+        .attr(
+          "class",
+          "uk-button uk-margin uk-button-primary uk-border-rounded"
+        )
         .text("Get another random image")
         .on("click", () => {
-          d3.selectAll("h3").remove();
-          d3.selectAll("img").remove();
-          d3.selectAll("button").remove();
-          d3.selectAll("form").remove();
-          getOne(url);
+          d3.selectAll(".random-image").remove();
+          getFavs(localFavs);
+          getOne(oneRandom);
+        });
+
+      oneImage
+        .append("div")
+        .selectAll("button")
+        .data(buildBreedList(breeds))
+        .enter()
+        .append("button")
+        .attr(
+          "class",
+          "uk-button-default uk-button-mini uk-margin-small uk-margin-small-right"
+        )
+        .text(d => `random ${d}`)
+        .on("click", d => {
+          d3.event.preventDefault();
+
+          const clean = d.trim().includes(" ")
+            ? `${d.substring(d.search(" ") + 1)}/${d.substring(
+                0,
+                d.search(" ")
+              )}`
+            : d;
+
+          d3.selectAll(".random-image").remove();
+          getFavs(JSON.parse(localStorage.getItem("favorites")));
+          getOne(randomByBreed(clean));
         });
     })
     .catch(err => console.log("failed to load one random image.", err));
 }
 
-fetch(breedList)
-  .then(res => {
-    return res.json();
+function getFavs(myFavs) {
+  const favs = d3.select("#favs");
+
+  favPic = favs
+    .selectAll(".fav-pics")
+    .data(myFavs)
+    .enter()
+    .append("div")
+    .attr(
+      "class",
+      "fav-pics uk-margin uk-margin-right uk-display-inline-block "
+    )
+    .insert("h3")
+    .text(d => `Rating: ${d.rating}`)
+    .insert("img")
+    .attr("height", 200)
+    .attr("width", 200)
+    .attr("class", "dog-image uk-border-rounded uk-padding-small")
+    .attr("src", d => d.url)
+    .on("click", function() {
+      d3.select(this).classed(
+        "uk-width-auto",
+        this.classList.contains("uk-width-auto") ? "" : "uk-width-auto"
+      );
+    });
+}
+
+function getBreeds(url) {
+  fetch(url, {
+    method: "get",
+    mode: "cors"
   })
-  .then(res => {
-    console.log(res.message);
-  })
-  .catch(err => console.log(`Breed list failed!`, err));
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      breeds = res.message;
+    })
+    .catch(err => console.log(`error fetching the breed list:`, err));
+}
+
+function buildBreedList(obj) {
+  let listOfBreeds = [];
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const element = obj[key];
+      if (element.length > 1) {
+        const newElement = element.map(item => `${item} ${key}`);
+        listOfBreeds = [...listOfBreeds, ...newElement];
+      } else {
+        if (key.length > 1) {
+          listOfBreeds = [...listOfBreeds, key];
+        }
+      }
+    }
+  }
+  return listOfBreeds;
+}
 
 getOne(oneRandom);
+getFavs(JSON.parse(localStorage.getItem("favorites")));
+getBreeds(breedList);
